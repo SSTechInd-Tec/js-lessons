8965cfe4eabb2e674dad8a79946ce2c5326e9ae2
starkbak <sstechnologyindtec@gmail.com>
2024-04-20 19:43:31 +0430

    intro

---
+++ script1.js
@@ -0,0 +1,19 @@
+
+/* 
+    data types and variables
+    operators
+    statements
+        condition
+        loop
+        function
+        classes
+        error handling
+    modules
+    files
+
+*/
+
+document.getElementById('header1').onmouseleave = function() {
+    const name = prompt('Enter Your Name: ')
+    alert("Welcome "+name)
+}
